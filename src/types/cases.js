49679a763a54a13d2c4b727bceb8dafdf0d60725
Tony <antoine.banck@gmail.com>
2016-10-24 15:22:19 +0200

    toLower vs. toUpper

---
+++ src/types/cases.js
@@ -0,0 +1,11 @@
+// tolower vs toupper
+
+var string = 'My name is Bond'
+
+//to upper case : tous les caractères de la chaine deviennent des majuscules
+var toUpper = string.toUpperCase();
+console.log('La phrase "',string, '" en majuscule, donne : "',toUpper,'".');
+
+//to lower case : tous les caractères de la chaine deviennent des minuscules
+var toLower = string.toLowerCase();
+console.log('La phrase "',string, '" en minuscule, donne : "',toLower,'".');
